48c3243714c13572f920ffbb575aff218d17c677
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-22 13:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let currentt=document.getElementById("timer");
+let currentD=new Date();
+setInterval(
+	function(){
+		currentD=new Date();
+		currentt.innerHTML=currentD.toLocaleString();
+	},1000
+)
